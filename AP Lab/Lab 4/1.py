62583cc115bd7e6321f51145b45d45f407f4e907
Devang Arora <devangraj.arora@gmail.com>
2020-12-09 13:41:57 +0530

    ez stuff

---
+++ AP Lab/Lab 4/1.py
@@ -0,0 +1,5 @@
+import math
+n = int(input())
+print("SIN: ", math.sin(math.radians(n))) # n in degrees
+print("SQRT: ", math.sqrt(n))
+print("LOG: ", math.log(n))
---
+++ AP Lab/Lab 4/2.py
@@ -0,0 +1,5 @@
+import cmath
+n = complex(input())
+print("SIN: ", cmath.sin(n)) # n in degrees
+print("SQRT: ", cmath.sqrt(n))
+print("LOG: ", cmath.log(n))
